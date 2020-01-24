4f3567dc49bad90ff8899fc42d5578c0bef63717
grantmacken <grantmacken@gmail.com>
2020-01-24 18:05:34 +1300

    correst certs path

--- proxy/.inc/certs.mk
+++ proxy/.inc/certs.mk
@@ -93,15 +93,16 @@ certsToHost:
 	# dh param in different location
 	@gcloud compute ssh $(GCE_NAME) --command  'docker cp or:$(LETSENCRYPT)/dh-param.pem ./live/$(TLS_COMMON_NAME) -L'
 	@gcloud compute ssh $(GCE_NAME) --command 'ls -al ./live/$(TLS_COMMON_NAME)'
-	@sudo mkdir -p $(LETSENCRYPT)
-	@sudo chown ${USER} $(LETSENCRYPT)
-	@gcloud compute scp  $(GCE_NAME):~/live $(LETSENCRYPT) --recurse
+	@echo '---------------------------------------------------------------------'
+	@#sudo mkdir -p $(LETSENCRYPT)
+	@#sudo chown ${USER} $(LETSENCRYPT)
+	@#gcloud compute scp  $(GCE_NAME):~/live $(LETSENCRYPT) --recurse
 	# clean up on GCE Host
-	@gcloud compute ssh $(GCE_NAME) --command 'rm -r ./live'
+	@#gcloud compute ssh $(GCE_NAME) --command 'rm -r ./live'
 	# relocate dh param
-	@mv $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)/dh-param.pem $(LETSENCRYPT)/dh-param.pem
-	@ls -al $(LETSENCRYPT)
-	@ls -al $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)
+	@# mv $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)/dh-param.pem $(LETSENCRYPT)/dh-param.pem
+	@#ls -al $(LETSENCRYPT)
+	@#ls -al $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)
 	# create letsencrypt volume
 	@docker volume create --driver local --name letsencrypt
 	# create a dummy container and attach to letsencypt volume
@@ -109,11 +110,14 @@ certsToHost:
  --mount type=volume,target=$(LETSENCRYPT),source=letsencrypt \
  --entrypoint "/usr/bin/tail" $(PROXY_DOCKER_IMAGE)  -f /dev/null
 	# copy retrieved certs into letsencrypt volume
-	@docker cp $(LETSENCRYPT)/live/$(TLS_COMMON_NAME) dummy:$(LETSENCRYPT)/live 
-	@docker cp $(LETSENCRYPT)/dh-param.pem dummy:$(LETSENCRYPT)
+	@docker exec dummy mkdir -p $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)
+	@docker cp ./live/$(TLS_COMMON_NAME)/. dummy:$(LETSENCRYPT)/live/$(TLS_COMMON_NAME)
+	@docker mv $(LETSENCRYPT)/dh-param.pem $(LETSENCRYPT)/
 	# view created items
-	@docker exec dummy ls $(LETSENCRYPT) | grep 'dh-param.pem'
-	@docker exec dummy ls $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)
+	@echo '---------------------------------------------------------------------'
+	@docker exec dummy ls -al $(LETSENCRYPT) | grep 'dh-param.pem'
+	@docker exec dummy ls -al $(LETSENCRYPT)/live/$(TLS_COMMON_NAME)
+	@echo '---------------------------------------------------------------------'
 	@docker stop dummy
 	# adjust hosts file TODO all domains
 	@echo "127.0.0.1  $(TLS_COMMON_NAME)" | sudo tee -a /etc/hosts
