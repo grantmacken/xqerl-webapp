77a016573aed657dca256095361bd24b1591bbf1
grantmacken <grantmacken@gmail.com>
2020-01-24 14:27:53 +1300

    gcloud setup and lets encypt volumew

---
+++ proxy/lualib/access.lua
@@ -0,0 +1,13 @@
+local _M = {}
+_M.version = '0.0.2'
+
+--[[
+https://indielogin.com/api
+If everything is successful, the user will be redirected back to the redirect_uri 
+you specified in the form. You'll see two parameters in the query string, state 
+and code. Check that the state matches the value you set originally before continuing.
+
+--]]
+
+
+return _M
