788af0cb63ee035764a08a8819ebef9cf214b817
grantmacken <grantmacken@gmail.com>
2020-02-03 18:38:07 +1300

    check volumes

--- site/.inc/gcloud.mk
+++ site/.inc/gcloud.mk
@@ -11,6 +11,17 @@ GcloudVolumeCreate = grep -q $1 $(2) || $(Gcmd) 'docker volume create --driver l
 	@$(Gcmd) '$(call MustHaveVolume,xqerl-database)'
 	@$(Gcmd) '$(call MustHaveVolume,static-assets)'
 
+.PHONY: gcloud-assets-volume-deploy
+gcloud-assets-volume-deploy: $(D)/static-assets.tar
+	@gcloud compute ssh $(GCE_NAME) --command  'mkdir -p $(D)'
+	@gcloud compute scp $< $(GCE_NAME):~/$(D)
+	@gcloud compute ssh $(GCE_NAME) --command \
+ 'docker run --rm \
+ --mount $(MountAssets) \
+ --mount type=bind,target=/tmp,source=/home/$(GCE_NAME)/$(D) \
+ --entrypoint "tar" $(PROXY_DOCKER_IMAGE) xvf /tmp/$(notdir $<) -C /'
+
+
 # this is call from github action but may be called locally
 .PHONY: gcloud-code-volume-deploy
 gcloud-code-volume-deploy: $(D)/xqerl-compiled-code.tar
@@ -71,51 +82,5 @@ gcloud-show-lib-namespaces:
 	@$(GCmd) \
  "xqerl eval 'xqerl_code_server:library_namespaces()'"
 
-.PHONY: gcloud-assets-volume-deploy
-gcloud-assets-volume-deploy: $(D)/static-assets.tar
-	@gcloud compute ssh $(GCE_NAME) --command  'mkdir -p $(D)'
-	@gcloud compute scp $< $(GCE_NAME):~/$(D)
-	@gcloud compute ssh $(GCE_NAME) --command \
- 'docker run --rm \
- --mount $(MountAssets) \
- --mount type=bind,target=/tmp,source=/home/$(GCE_NAME)/$(D) \
- --entrypoint "tar" $(PROXY_DOCKER_IMAGE) xvf /tmp/$(notdir $<) -C /'
 
-#######################
-# GCLOUD DEPLOYMENT ###
-#######################
-Gcmd := gcloud compute ssh $(GCE_NAME) --command
-GCmd := gcloud compute ssh $(GCE_NAME) --container $(XQ) --command 
-
-.PHONY: gcloud-conf-volume-deploy
-gcloud-conf-volume-deploy: $(D)/nginx-configuration.tar
-	@# make sure we have a deploy directory on the host
-	@gcloud compute ssh $(GCE_NAME) --command  'mkdir -p $(D)'
-	@# copy into GCE host
-	@gcloud compute scp $< $(GCE_NAME):~/$(D)
-	@#gcloud compute ssh $(GCE_NAME) --command  'ls -al ./deploy'
-	@# extract tar into volume
-	@gcloud compute ssh $(GCE_NAME) --command \
- 'docker run --rm \
- --mount $(mountNginxConf) \
- --mount type=bind,target=/tmp,source=/home/$(GCE_NAME)/$(D) \
- --entrypoint "tar" $(PROXY_DOCKER_IMAGE) xvf /tmp/$(notdir $<) -C /'
-
-GcloudVolumeCreate = grep -q $(1) $(2) || $(Gcmd) 'docker volume create --driver local --name $(1)'
-
-.PHONY: gcloud-check-volumes
-gcloud-check-volumes: $(D)/gcloud-volume-check.txt
-	@$(foreach volume,$(VolumeList),$(call GcloudVolumeCreate,$(volume),$(<)))
-
-$(D)/gcloud-volume-check.txt: 
-	@mkdir -p $(dir $@)
-	@$(Gcmd) 'docker volume list  --format "{{.Name}}"' > $@
-
-.PHONY: gcloud-proxy-up
-gcloud-proxy-up: 
-	@echo '##[ $@ ]##'
-	@$(Gcmd) 'docker ps --filter name=$(OR) --format "{{.Status}}"' | grep -q Up || $(Gcmd) '$(dkrRun)'
-	@sleep 2
-	@$(Gcmd) 'docker ps --filter name=$(OR) --format "{{.Status}}"'
-	@$(Gcmd) 'docker logs $(OR)'
 
